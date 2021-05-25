21ca48e623637cc7a362980a5f30bec0f355e54e
emily doswell <emilydoswell@yahoo.co.uk>
2021-05-25 17:28:28 +0200

    added links to create page

--- config/routes.rb
+++ config/routes.rb
@@ -1,16 +1,17 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  # See all Tasks
-  get '/tasks', to: 'tasks#index'
-  
-  # Add a new task
-  get 'tasks/new', to: 'tasks#new'
+    # See all Tasks
+    # get '/tasks', to: 'tasks#index'
+    
+    # # Add a new task
+    # get 'tasks/new', to: 'tasks#new'
 
-  # View the details of a task
-  get '/tasks/:id', to: 'tasks#show', as: :task
-  post '/tasks', to: 'tasks#create'
+    # # View the details of a task
+    # get '/tasks/:id', to: 'tasks#show', as: :task
+    # post '/tasks', to: 'tasks#create'
 
-  # Remove a task
-  delete "tasks/:id", to: "tasks#destroy"
+    # # Remove a task
+    # delete "tasks/:id", to: "tasks#destroy"
 
+  resources :tasks
 end
